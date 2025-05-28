125b4576837fd7a33a91fd33d765ecb2028f3f5f
Peter Ashaya <peterashaya@gmail.com>
2025-05-28 14:58:26 +0100

    Update build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,6 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh QuadroM2200.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh 
+LenovoP51.QuadroM2200.4096.84067d0003.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
